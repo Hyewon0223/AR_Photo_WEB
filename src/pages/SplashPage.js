ea5050fcaa10c68782f0119b2b535d801ee30de2
Hyewon0223 <ch2w2006@kookmin.ac.kr>
2022-05-28 19:37:26 +0900

    [Refactor] BackgroundImg

--- src/pages/SplashPage.js
+++ src/pages/SplashPage.js
@@ -6,8 +6,8 @@ import PhotoBubble from '../assets/PHOTOBUBBLE.png';
 export const SplashPage = () => {
     const navigate = useNavigate();
 
-    return <Container imgURL={SplashImg} onClick={()=>navigate('/NumOfPeople')}>
-        {/*<Title>AR PHOTO</Title>*/}
+    return <Container onClick={()=>navigate('/NumOfPeople')}>
+        <BackgroundImg imgURL={SplashImg} />
         <Title imgURL={PhotoBubble}/>
         <SubTitle>화면을 터치해주세요!</SubTitle>
     </Container>
@@ -21,15 +21,24 @@ const Container = styled.div`
   align-items: center;
   gap: 25px;
   cursor: pointer;
-  background: url(${props=>props.imgURL}) no-repeat 40% 25%, linear-gradient(198.19deg, rgba(212, 217, 255, 0.81) 3.45%, rgba(232, 236, 255, 0.81) 28.13%, rgba(145, 183, 255, 0.81) 75.96%), linear-gradient(180deg, rgba(229, 241, 255, 0.2) 0%, rgba(221, 229, 250, 0.2) 100%), #E1E0E0;
+  background: 40% 25%, linear-gradient(198.19deg, rgba(212, 217, 255, 0.81) 3.45%, rgba(232, 236, 255, 0.81) 28.13%, rgba(145, 183, 255, 0.81) 75.96%), linear-gradient(180deg, rgba(229, 241, 255, 0.2) 0%, rgba(221, 229, 250, 0.2) 100%), #E1E0E0;
   background-blend-mode: overlay, normal, normal;
 `
-
+const BackgroundImg = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 1;
+  background: url(${props=>props.imgURL}) no-repeat center 20%;
+`
 const Title = styled.div`
   height: 110px;
   width: 971px;
   background-size: cover;
   background: url(${props=>props.imgURL}) no-repeat;
+  z-index: 10;
 `
 
 const SubTitle = styled.div`
