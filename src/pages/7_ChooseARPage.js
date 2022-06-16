dc4b13c7bdc5c2255df5dd22449d76ca33b97331
Hyewon0223 <ch2w2006@kookmin.ac.kr>
2022-06-17 02:17:05 +0900

    [Feature] Choose AR Page

--- src/pages/7_ChooseARPage.js
+++ src/pages/7_ChooseARPage.js
@@ -1,33 +1,44 @@
-import {useLocation, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import BaseElement from "../components/BaseElement";
 import {Camera} from "react-camera-pro";
 import React, {useRef, useState} from "react";
 import styled from "styled-components";
-import {Category, CategoryContainer, DetailBox, DetailContainer, SelectWrapper} from "./11_SelectFrame";
+import {Category, CategoryContainer, SelectWrapper} from "./11_SelectFrame";
 import {SelectCheck} from "./1_PeopleNumPage";
 import check from "../assets/Check.png";
+import arPink from "../assets/ar_pink.png";
+import arPhotoBubble from "../assets/ar_photoBubble.png";
+import arEx from "../assets/ar_ex.png";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import person from "../assets/Person.png";
+import personActive from "../assets/ActivePerson.png"
 
 export const ChooseARPage = () => {
     const navigate = useNavigate();
-    const location = useLocation();
     const camera = useRef(null);
-    const [image, setImage] = useState(null);
     const [category, setCategory] = useState(0);
-    const [detail, setDetail] = useState(["밝음", "어두움", "기본"]);
+    const [detail, setDetail] = useState([arPink,arPink,arPink,arPink,arPink,arPink]);
     const [select, setSelect] = useState();
-
+    const [personActive, setPersonActive] = useState(0);
+    const settings = {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+    };
     const data = [
         {
             category: "머리띠",
-            detail : ["밝음", "어두움", "기본"]
+            detail : [arPink,arPink,arPink,arPink,arPink,arPink, arPink,arPink]
         },
         {
             category: "안경",
-            detail : ["흑백1","흑백2","흑백3"]
+            detail : [arPhotoBubble,arPhotoBubble,arPhotoBubble]
         },
         {
             category: "이모지",
-            detail : ["어쩌구1", "어쩌구2", "어쩌구3"]
+            detail : [arEx, arEx, arEx,arEx,arEx,arEx,arEx]
         },
     ]
     const onCategoryClick = (idx, detail) => {
@@ -36,41 +47,57 @@ export const ChooseARPage = () => {
     }
     const onBoxClick = (idx) => {
         setSelect(idx);
-        localStorage.setItem("filter1",category);
-        localStorage.setItem("filter2",detail);
+        localStorage.setItem("arCategory",category);
+        localStorage.setItem("arDetail",detail);
         setTimeout(function () {
             // navigate("/SelectFilter", {state: location.state});
         }, 500);
     }
     return (
-        <BaseElement isTimer onBackClick={()=>navigate("/SelectAR")}>
+        <BaseElement isTimer onBackClick={()=>navigate("/SelectAR")} etc={select !== undefined && "다음"} onEtcClick={()=>navigate("/Camera")}>
             <CameraContainer>
                 <CameraWrapper>
-                    <Camera ref={camera} aspectRatio={4/3} errorMessages="error"/>
+                    <Camera ref={camera} aspectRatio={5/3} errorMessages="error"/>
                 </CameraWrapper>
                 <ApplyContainer>
                     <MainText>누구에게 소품을 입힐까요?</MainText>
                     <SubText>서있는 순서대로 소품이 적용됩니다.</SubText>
+                    <PersonWrapper>
+                        {[0,1,2].map((active, idx)=>{
+                            return <Person isActive={personActive===idx} onClick={()=>setPersonActive(idx)}>{idx}</Person>
+                        })}
+                    </PersonWrapper>
                 </ApplyContainer>
             </CameraContainer>
             <SelectWrapper>
-                <CategoryContainer mb="93px">
+                <ARCategoryContainer mb="41px">
                     {data.map((data,idx)=>{
-                        return <Category isActive={category===idx} onClick={()=>onCategoryClick(idx, data.detail)}>{data.category}</Category>
+                        return <ARCategory isActive={category===idx} onClick={()=>onCategoryClick(idx, data.detail)}>{data.category}</ARCategory>
                     })}
-                </CategoryContainer>
+                </ARCategoryContainer>
 
-                <DetailContainer>
+                <ARContainer>
                     {detail.map((data, idx)=>{
-                        return <DetailBox onClick={()=>onBoxClick(idx)} isActive={select===idx}>
-                            {data}
+                        return <ARBox onClick={()=>onBoxClick(idx)} isActive={select===idx} imgUrl={data}>
+                            <BackImg imgUrl={data}/>
                             {select===idx ?
                                 <SelectCheck src={check}/>
                                 : null
                             }
-                        </DetailBox>
+                        </ARBox>
                     })}
-                </DetailContainer>
+                </ARContainer>
+                {/*<StyledSlider {...settings}>*/}
+                {/*    {detail.map((img,idx)=>{*/}
+                {/*        return <ARBox onClick={()=>onBoxClick(idx)} isActive={select===idx}>*/}
+                {/*            <BackImg imgUrl={img}/>*/}
+                {/*            {select===idx ?*/}
+                {/*                <SelectCheck src={check}/>*/}
+                {/*                : null*/}
+                {/*            }*/}
+                {/*        </ARBox>*/}
+                {/*    })}*/}
+                {/*</StyledSlider>*/}
             </SelectWrapper>
         </BaseElement>
     );
@@ -78,23 +105,25 @@ export const ChooseARPage = () => {
 
 const CameraContainer = styled.div`
   margin: auto;
-  height: 30%;
-  width: 50%;
+  width: 80%;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 `
 const CameraWrapper = styled.div`
-  //margin: auto auto 25px;
-  width: 100%;
-  max-height: 90vh;
+  width: 50%;
   margin: auto;
 `
 const ApplyContainer = styled.div`
   background: #7798C9;
   backdrop-filter: blur(4px);
   border-radius: 50px;
+  height: 420px;
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 const MainText = styled.div`
   font-family: 'ONE Mobile POP', sans-serif;
@@ -111,4 +140,69 @@ const SubText = styled.div`
   font-size: 26px;
   line-height: 31px;
   color: rgba(255, 255, 255, 0.61);
+`
+const PersonWrapper = styled.div`
+  gap: 15px;
+  margin-top: 45px;
+  display: flex;
+  justify-content: center;
+`
+const Person = styled.div`
+  width: 120px;
+  height: 132px;
+  background: url(${props=>props.isActive? personActive:person}) no-repeat center;
+  background-size: cover;
+  font-family: 'ONE Mobile POP', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 45px;
+  line-height: 50px;
+  color: ${props=>props.isActive? "#5A79C7":"#FFFFFF"};
+  display: flex;
+  justify-content: center;
+  padding-top: 10px;
+  cursor: pointer;
+`
+const ARCategoryContainer = styled(CategoryContainer)`
+  justify-content: flex-start;
+`
+const ARCategory = styled(Category)`
+  width: 170px;
+`
+const ARBox = styled.div`
+  position: relative;
+  background: ${props=>props.isActive? 'rgba(119, 152, 201, 0.78)' : "linear-gradient(196.1deg, rgba(255, 255, 255, 0.61) -15.99%, rgba(255, 255, 255, 0.352657) 101.75%, rgba(255, 255, 255, 0) 142.09%), linear-gradient(80.77deg, #FBFBFD 27.85%, rgba(251, 251, 253, 0) 91.1%)"};
+  border: 5px solid ${props=>props.isActive?"#678AC0":"#FFFFFF"};
+  border-radius: 32px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 150px;
+`
+const StyledSlider = styled(Slider)`
+  .slider .slick-list {
+    margin:0 -20px;
+  }
+  .slick-slide {
+    margin:0 10px;
+  }
+`;
+const BackImg = styled.div`
+  position: relative;
+  border-radius: 32px;
+  width: 150px;
+  padding-top: 150px;
+  background: url(${props=>props.imgUrl}) no-repeat center;
+  background-size: cover;
+  margin: auto;
+`
+const ARContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 20px;
+  width: 100%;
+  overflow: auto;
 `
